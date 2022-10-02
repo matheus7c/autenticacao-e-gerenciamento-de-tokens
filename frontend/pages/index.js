49869eb33d2f62ce9a7afe7a62da5770572e01fe
Matheus Castro do Nascimento <46095722+matheus7c@users.noreply.github.com>
2022-10-02 18:30:01 -0300

    Definindo paginas e adição do router

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,20 +1,47 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { route } from "next/dist/server/router";
+
 export default function HomeScreen() {
+  const router = useRouter();
+  const [values, setValues] = React.useState({
+    user: "matheus",
+    pwd: "123456",
+  });
+
+  function handleChange(event) {
+    const fieldValue = event.target.value;
+    const fieldName = event.target.name;
+    setValues((currentValue) => {
+      return { ...currentValue, [fieldName]: fieldValue };
+    });
+  }
+
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          router.push("/auth-page-ssr");
+        }}
+      >
         <input
-          placeholder="Usuário" name="usuario"
-          defaultValue="omariosouto"
+          placeholder="Usuário"
+          name="user"
+          value={values.user}
+          onChange={handleChange}
         />
         <input
-          placeholder="Senha" name="senha" type="password"
-          defaultValue="safepassword"
+          placeholder="Senha"
+          name="pwd"
+          type="password"
+          value={values.pwd}
+          onChange={handleChange}
         />
+        {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
         <div>
-          <button>
-            Entrar
-          </button>
+          <button>Entrar</button>
         </div>
       </form>
     </div>
